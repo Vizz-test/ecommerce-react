a4c2b1fde9584345ba5dd47c5259e0cb809468dc
irfan-vizz <engr.irfanali970@gmail.com>
2022-09-20 15:08:58 +0500

    irfan change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Zeeshan
+          Learn React with Irfan
         </a>
       </header>
     </div>
